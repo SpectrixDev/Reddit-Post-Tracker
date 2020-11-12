b215a0d48e01dd3f006ef45fb2d2816ce4e2c2db
Spectrix <owenhutch123@gmail.com>
2020-11-12 13:55:32 +0200

    wasnt a smart idea

--- scripts/scorehandler.js
+++ scripts/scorehandler.js
@@ -16,46 +16,6 @@ function transition(){
 };
 
 function updateScore(score) {
-document.getElementById('counter').innerHTML = score;
-
-var dps = []; // dataPoints
-var chart = new CanvasJS.Chart("chartContainer", {
-	title :{
-		text: "Dynamic Data"
-	},
-	data: [{
-		type: "line",
-		dataPoints: dps
-	}]});
-
-var xVal = 0;
-var yVal = 100; 
-var updateInterval = 1000;
-var dataLength = 20; // number of dataPoints visible at any point
-
-
-var updateChart = function (count) {
-
-	count = count || 1;
-
-	for (var j = 0; j < count; j++) {
-		yVal = yVal + score;
-		dps.push({
-			x: xVal,
-			y: yVal
-		});
-		xVal++;
-	}
-
-	if (dps.length > dataLength) {
-		dps.shift();
-	}
-
-	chart.render();
-};
-
-updateChart(dataLength);
-setInterval(function(){updateChart()}, updateInterval);
-
-var t = setTimeout(getURLthenScore, 5000);
+	document.getElementById('counter').innerHTML = score;
+	var t = setTimeout(getURLthenScore, 5000);
 };
