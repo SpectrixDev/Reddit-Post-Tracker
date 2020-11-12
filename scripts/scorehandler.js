1250b9ba0cc611d3e9447dea65b350d494b6517d
Spectrix <owenhutch123@gmail.com>
2020-11-12 13:49:27 +0200

    using github as vscode at this point

--- scripts/scorehandler.js
+++ scripts/scorehandler.js
@@ -12,7 +12,7 @@ function transition(){
     document.getElementById("textbox").classList.add('zoomOut');
     document.getElementById("buttondiv").classList.add('fadeOutDown');
     document.getElementsByTagName("H2")[0].removeAttribute("hidden");
-    document.getElementsById("chartContainer").removeAttribute("hidden");
+    document.getElementById("chartContainer").removeAttribute("hidden");
 };
 
 function updateScore(score) {
